c2705352bf27246c89b3dd7593273476c0d4f7eb
tonymk0987 <kamandetonymuriuki@gmail.com>
2024-07-17 00:24:41 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -37,4 +37,5 @@ class BakedGood(db.Model, SerializerMixin):
     bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
 
     def __repr__(self):
+        
         return f'<Baked Good {self.name}, ${self.price}>'
